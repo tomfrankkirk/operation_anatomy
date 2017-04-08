16712173c35ad96a53aac3f3e7ee7837150292c8
= <tomfrankkirk@gmail.com>
2017-04-08 19:48:39 +0200

    Updates to seed (solution is always possibleSolutions[0])

--- db/seeds.rb
+++ db/seeds.rb
@@ -3,15 +3,14 @@
 
 # If no users exist, create one
 if User.all == []
-    User.create!(email: "tomfrankkirk@gmail.com", password: "password")
+    User.create!(email: "tomfrankkirk@gmail.com", password: "password", isAdmin: true)
 end
 
 # Delete all questions to reseed them
 # Don't delete topics - we need to keep this to keep the ids. 
 Question.delete_all
-
 # Seeding topics now. 
-shoulderTopic = Topic.create(name: "Shoulder", display_name: "The Shoulder Joint")
+# shoulderTopic = Topic.create(name: "Shoulder", display_name: "The Shoulder Joint")
 
 # Load the level names in. Each topic directory should have a document named LevelNames.txt at the top level. 
 Topic.all.each do |t|
@@ -25,4 +24,6 @@ File.open(Dir['teaching/questionSeed.rb'].first) { |file| load(file) }
 DictionaryEntry.all.each do |e|
     e.destroy
 end 
+
+# Level names for each topic. 
 File.open(Dir['teaching/JSDict.rb'].first) { |file| load(file) }
