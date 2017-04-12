25f4aec44bc3b9d38b8efa3ec6f4974975575b79
= <tomfrankkirk@gmail.com>
2017-04-12 12:17:07 +0100

    Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170407103710) do
+ActiveRecord::Schema.define(version: 20170409105656) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20170407103710) do
     t.integer  "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.boolean  "solved"
     t.index ["user_id"], name: "index_feedback_records_on_user_id", using: :btree
   end
 
