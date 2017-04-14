26e40a0d9177e1a1aa7dde1a2b0d2fc52caeba5f
= <tomfrankkirk@gmail.com>
2017-04-14 08:38:48 +0100

    More heroku setup

--- db/seeds.rb
+++ db/seeds.rb
@@ -2,16 +2,14 @@
 # The data can then be loaded with the rails db:seed command (or created alongside the database with db:setup).
 
 # If no users exist, create one
-if User.all == []
-    User.create(email: "tomfrankkirk@gmail.com", password: "password", isAdmin: true)
-    User.create(email: "christopher.horton@gtc.ox.ac.uk", password: "AnatomicalWalkthrough")
-end
+User.create(email: "tomfrankkirk@gmail.com", password: "password", isAdmin: true)
+User.create(email: "christopher.horton@gtc.ox.ac.uk", password: "AnatomicalWalkthrough")
 
 # Delete all questions to reseed them
-# Don't delete topics - we need to keep this to keep the ids. 
 Question.delete_all
+
 # Seeding topics now. 
-# shoulderTopic = Topic.create(name: "Shoulder", display_name: "The Shoulder Joint")
+shoulderTopic = Topic.create(name: "Shoulder", display_name: "The Shoulder Joint")
 
 # Load the level names in. Each topic directory should have a document named LevelNames.txt at the top level. 
 Topic.all.each do |t|
