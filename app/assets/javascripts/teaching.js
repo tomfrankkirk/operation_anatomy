0185d1a675f2eac7189e095b2e5b5260a75bf5a1
= <tomfrankkirk@gmail.com>
2017-05-07 21:11:36 +0100

    Define now fires on selection change, with a time block to stop double defines

--- app/assets/javascripts/teaching.js
+++ app/assets/javascripts/teaching.js
@@ -1,34 +1,61 @@
-document.addEventListener("selectionchange", define, false)
+var lastDfnTime = new Date(); 
+
+$(document).ready(function () {  
+
+    document.addEventListener("selectionchange", function () {
+        console.log("selectionchange event caught")
+
+        // Search for the pageDisplay div, if found then send off the request.  
+        div = document.getElementById("pageDisplayArea"); 
+        now = new Date(); 
+        console.log("time diff" + (now - lastDfnTime))
+        if (div != null && now - lastDfnTime > 3000) {
+            define(); 
+        } else {
+            console.log("Could not find pageDisplayArea div, event ignored."); 
+        }
+
+    }, false);
+        
+});
 
 function define() {
     searchString = getSelectionText();
     if (searchString.length > 1 && searchString.length < 35) {
         console.log("Requesting definition for text: " + searchString)
         $.ajax({
-                url: 'teaching/define',
-                data: { searchString: searchString },
-                type: 'GET',
-                dataType: 'json',
+            url: 'teaching/define',
+            data: { searchString: searchString },
+            type: 'GET',
+            dataType: 'json',
                 
-                success: function(data, textStatus, jqXHR) {
-                    if (data.definition != "") {
-                        stringToDisplay = data.title + ": "
-                        stringToDisplay = stringToDisplay + data.definition
-                        if (data.example) {
-                            stringToDisplay = stringToDisplay + "\n\nExample: " + data.example
-                        }
-                        window.alert(stringToDisplay)
-                    } else {
-                        console.log("Request returned with nil definition.")
+            success: function (data, textStatus, jqXHR) {
+                if (data.definition != "") {
+                    stringToDisplay = data.title + ": "
+                    stringToDisplay = stringToDisplay + data.definition
+                    if (data.example) {
+                        stringToDisplay = stringToDisplay + "\n\nExample: " + data.example
                     }
-                    data = null; 
-                },
-                
-                error: function(jqXHR, textStatus, errorThrown) {
-                    console.log("AJAX error: #{textStatus}")
+                    window.alert(stringToDisplay)
+                    // Clear the selection set the new time flag
+                    clearSelection();
+                    lastDfnTime = new Date(); 
+
+                } else {
+                    console.log("Request returned with nil definition.")
                 }
+            },
+                
+            error: function (jqXHR, textStatus, errorThrown) {
+                console.log("AJAX error: #{textStatus}")
+            }
         })
     }
+}
+
+function clearSelection() {
+    if (window.getSelection) window.getSelection().removeAllRanges();
+    else if (document.selection) document.selection.empty();
 }
 
 function sayHello() { window.alert("Hello!") }; 
