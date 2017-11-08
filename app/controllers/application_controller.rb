627078d4eb46338b9c81d0f42668947dd4e2b60a
Tom Kirk <tomfrankkirk@gmail.com>
2017-11-08 18:29:44 +0000

    changed signin path

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,5 +1,10 @@
 require "browser"
 
 class ApplicationController < ActionController::Base
-  protect_from_forgery with: :exception
+   protect_from_forgery with: :exception
+
+   def after_sign_in_path_for(resource)
+      systems_path 
+   end
+
 end
