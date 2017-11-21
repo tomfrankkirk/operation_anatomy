dd7d6f5996bf8f89f4afd3a57107a79168724152
Tom Kirk <tomfrankkirk@gmail.com>
2017-11-21 23:07:37 +0000

    migration: display name for systems

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171108182442) do
+ActiveRecord::Schema.define(version: 20171121215949) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -47,8 +47,9 @@ ActiveRecord::Schema.define(version: 20171108182442) do
 
   create_table "systems", force: :cascade do |t|
     t.string   "name"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
+    t.datetime "created_at",   null: false
+    t.datetime "updated_at",   null: false
+    t.string   "display_name"
   end
 
   create_table "topics", force: :cascade do |t|
