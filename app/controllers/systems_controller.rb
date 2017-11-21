c70f915d76ccddd02d9477ca36781a392e8ea1c1
Tom Kirk <tomfrankkirk@gmail.com>
2017-11-21 17:51:07 +0000

    Index and show methods in systems

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,2 +1,13 @@
 class SystemsController < ApplicationController
+
+   def index 
+      @systems = System.all
+   end 
+
+   def show 
+      @system = System.find(params[:id])
+      @topics = @system.topics 
+   end
+   
+
 end
