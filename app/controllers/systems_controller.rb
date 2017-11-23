edae441d776cd7305b890b0b4748d3cd44efbc3a
Tom Kirk <tomfrankkirk@gmail.com>
2017-11-23 15:37:19 +0000

    controllers updated to use the new icon table partial

--- app/controllers/systems_controller.rb
+++ app/controllers/systems_controller.rb
@@ -1,12 +1,19 @@
-class SystemsController < ApplicationController
+class SystemsController < EndUserController
 
    def index 
       @systems = System.all
+      @systemNames = @systems.map { |s| s.name }
+      @pathStubs = @systems.map { |s| '/' + s.id.to_s }
+      @iconStubs = @systems.map { |s| s.shortName }
    end 
 
    def show 
       @system = System.find(params[:id])
-      @topics = @system.topics 
+      @topics = @system.topics
+      @topicNames = @topics.map { |t| t.name }
+      @pathStubs = @topics.map { |t| t.id.to_s }
+      @iconStubs = @topics.map { |t| t.shortName }
+      @path_root = topics_path + '/'
    end
    
 
--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -12,12 +12,16 @@ class TopicsController < EndUserController
     # Then rewrite the id in and continue. 
     def show 
         @topic = Topic.find(params[:id])
-        session[:forLevel] = nil 
-        session[:topicID] = @topic.id
         @revisionMode = current_user.revisionMode
         @admin = current_user.isAdmin && current_user.inAdminMode
         @maxLevel = current_user.checkLevelAccess(@topic.name)
         @highestViewed = current_user.getHighestViewedLevel(@topic.name)
+        @levelNames = @topic.levelNames 
+        @pathRoot = "/teaching?id=#{@topic.id}&forLevel="
+        @pathStubs = @topic.shortLevelNames
+        @iconStubs = @topic.shortLevelNames         
+        session[:forLevel] = nil 
+        session[:topicID] = @topic.id
     end
 
 end
