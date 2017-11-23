e2b34736363011f5e66399bc0af02549689c04bf
Tom Kirk <tomfrankkirk@gmail.com>
2017-11-23 15:42:01 +0000

    adding logic to replace "display_name" columns previously stored in the db

--- app/models/system.rb
+++ app/models/system.rb
@@ -1,4 +1,19 @@
 class System < ApplicationRecord
    has_many :topics
+   serialize :level_names, Array    
+
+   def shortNameForLevel(k)
+      long = self.level_names 
+      return long.split[0]
+   end 
+
+   def shortLevelNames 
+      longs = self.level_names 
+      return longs.map { |s| s.split[0].downcase }
+   end 
+
+   def shortName
+      return self.name.split[0].downcase 
+   end 
    
 end
--- app/models/topic.rb
+++ app/models/topic.rb
@@ -3,6 +3,18 @@ class Topic < ApplicationRecord
     belongs_to :system
     has_many :questions, dependent: :destroy
 
+    def shortName
+      splits = self.name.downcase.split
+      if splits[0] == "the"
+         return splits[1]
+      end 
+      return splits[0]
+   end 
+
+   def shortLevelNames 
+      return System.find(self.system_id).shortLevelNames
+   end 
+
     # Method to fetch shuffled array for all questions of a certain level within a topic. 
     # The IDs are then passed to a User, they will then fetch questions directly from 
     # the database by using each id as a key. 
@@ -22,33 +34,39 @@ class Topic < ApplicationRecord
         return (self.questions.where("level = ?", level)).count
     end 
 
+   
     def numberOfLevels
-        self.questions.maximum("level")
+      s = System.find(self.system_id)
+      return s.level_names.count
     end
 
     # Go hunting for the level names as a text file. If found, wipe the current ones
     # and then put in the latest ones. 
-    def loadLevelNames
-        path = Dir["teaching/#{self.name}/LevelNames.txt"]
-        if path
-            self.level_names = []
-            File.open(path.first) { |f|
-                lines = f.read
-                lines = lines.split("\n")
-                lines.each do |name|
-                    self.level_names << name 
-                end 
-            }
-            self.save
-        end
-    end
+   #  def loadLevelNames
+   #      path = Dir["teaching/#{self.name}/LevelNames.txt"]
+   #      if path
+   #          self.level_names = []
+   #          File.open(path.first) { |f|
+   #              lines = f.read
+   #              lines = lines.split("\n")
+   #              lines.each do |name|
+   #                  self.level_names << name 
+   #              end 
+   #          }
+   #          self.save
+   #      end
+   #  end
 
-    def levelName(forLevel)
-        if name = self.level_names[forLevel-1]
-            return name 
-        else
-            return "Level #{forLevel}"
-        end 
-    end 
+   #  def levelName(forLevel)
+   #      if name = self.level_names[forLevel-1]
+   #          return name 
+   #      else
+   #          return "Level #{forLevel}"
+   #      end 
+   #  end 
+
+   def levelNames 
+      return System.find(self.system_id).level_names 
+   end 
 
 end
