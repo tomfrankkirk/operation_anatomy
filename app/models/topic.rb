05fdf2d1d59a63f4e6a81e7f70ae9d6f953caf80
Tom Kirk <tomfrankkirk@gmail.com>
2017-12-13 22:17:30 +0000

    Naming convention change and also shift in indexing: levels will start at 1, not zero. This transformation happens on the topic methods, so calling levelName(1) returns the name for level0 in code terms

--- app/models/topic.rb
+++ app/models/topic.rb
@@ -15,12 +15,22 @@ class Topic < ApplicationRecord
       return System.find(self.system_id).shortLevelNames
    end 
 
+   def levelName(forLevel)
+      return self.shortLevelNames[forLevel - 1]
+   end
+
+   def levelNumber(forName)
+      return self.shortLevelNames.find_index(forName) + 1
+   end 
+
     # Method to fetch shuffled array for all questions of a certain level within a topic. 
     # The IDs are then passed to a User, they will then fetch questions directly from 
     # the database by using each id as a key. 
 
-    def fetchQuestionIDsForLevel(level)
+    def fetchQuestionIDsForLevel(levelName)
         # fetch all questions for the level
+        
+        level = self.levelNumber(levelName)
         questionObjects = self.questions.where("level = ?", level)
         questionIDs = []
         # extract each question ID. 
@@ -30,15 +40,18 @@ class Topic < ApplicationRecord
         questionIDs.shuffle
     end
 
-    def numberOfQuestionsInLevel(level)
-        return (self.questions.where("level = ?", level)).count
+    def numberOfQuestionsInLevel(levelName)
+        return qs = self.fetchQuestionIDsForLevel(levelName).count 
     end 
 
-   
     def numberOfLevels
       s = System.find(self.system_id)
       return s.level_names.count
     end
+
+    def levelNames 
+      return System.find(self.system_id).level_names 
+   end 
 
     # Go hunting for the level names as a text file. If found, wipe the current ones
     # and then put in the latest ones. 
@@ -65,8 +78,6 @@ class Topic < ApplicationRecord
    #      end 
    #  end 
 
-   def levelNames 
-      return System.find(self.system_id).level_names 
-   end 
+
 
 end
