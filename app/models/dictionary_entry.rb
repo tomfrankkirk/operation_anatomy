bf47d6bc21b891429759bb5ff06e45df96960a03
Tom Kirk <tomfrankkirk@gmail.com>
2017-12-13 22:16:08 +0000

    Search less potential options on a search string - weird defintions were coming back.

--- app/models/dictionary_entry.rb
+++ app/models/dictionary_entry.rb
@@ -1,44 +1,44 @@
 class DictionaryEntry < ApplicationRecord
 
-    def self.searchForEntry(wordToDefine)
+   def self.searchForEntry(wordToDefine)
 
-        # First, tidy up the input string, starting at the front. Remove whitespace or full stops.
-        while wordToDefine[0] == "." || wordToDefine[0] == " " || wordToDefine[0] == "-" 
-            wordToDefine = wordToDefine[1, wordToDefine.length]
-        end 
-        
-        # Now, tidy from the back.
-        while wordToDefine[wordToDefine.length] == "." || wordToDefine[wordToDefine.length] == " " || wordToDefine[wordToDefine.length] == "-"
-            wordToDefine = wordToDefine[0, wordToDefine.length - 1]
-        end 
-        
-        # Make lower case for case-insensitive comparison
-        wordToDefine = wordToDefine.downcase
-        
-        # Entry may or may not have a hyphen so wrap it up in wildcards.
-        entry = DictionaryEntry.find_by("title LIKE ?", + wordToDefine)
-        
-        # The input string may be too large: if it contains whitespace, split down and trial the parts.
-        # Maximum number of splits allowed is 4, to prevent entire paragraphs being fed in.
-        if !entry
-            substrings = wordToDefine.split(" ", 3);  
+      # First, tidy up the input string, starting at the front. Remove whitespace or full stops.
+      while wordToDefine[0] == "." || wordToDefine[0] == " " || wordToDefine[0] == "-" 
+         wordToDefine = wordToDefine[1, wordToDefine.length]
+      end 
+      
+      # Now, tidy from the back.
+      while wordToDefine[wordToDefine.length] == "." || wordToDefine[wordToDefine.length] == " " || wordToDefine[wordToDefine.length] == "-"
+         wordToDefine = wordToDefine[0, wordToDefine.length - 1]
+      end 
+      
+      # Make lower case for case-insensitive comparison
+      wordToDefine = wordToDefine.downcase
+      
+      # Entry may or may not have a hyphen so wrap it up in wildcards.
+      entry = DictionaryEntry.find_by("title LIKE ?", + wordToDefine)
+      
+      # The input string may be too large: if it contains whitespace, split down and trial the parts.
+      # Maximum number of splits allowed is 2, to prevent entire paragraphs being fed in.
+      if !entry
+         substrings = wordToDefine.split(" ", 2);  
 
-            substrings.each do |sub|
-                entry = DictionaryEntry.find_by("title LIKE ?", sub)
-                break if entry
-            end 
+         substrings.each do |sub|
+               entry = DictionaryEntry.find_by("title LIKE ?", sub)
+               break if entry
+         end 
 
-            # One last try: if nothing, try combos of the substrings. 
-            if substrings[0] && substrings[1] && !entry
-                entry = DictionaryEntry.find_by("title LIKE ?", substrings[0] + "%" + substrings[1]) 
-            end 
+         # One last try: if nothing, try combos of the substrings. 
+         if substrings[0] && substrings[1] && !entry
+               entry = DictionaryEntry.find_by("title LIKE ?", substrings[0] + "%" + substrings[1]) 
+         end 
 
-            if substrings[1] && substrings[2] && !entry
-                entry = DictionaryEntry.find_by("title LIKE ?", substrings[1] + "%" + substrings[2])
-            end 
+         if substrings[1] && substrings[2] && !entry
+               entry = DictionaryEntry.find_by("title LIKE ?", substrings[1] + "%" + substrings[2])
+         end 
 
-        end        
-        return entry
-    end 
+      end        
+      return entry
+   end 
 
 end
