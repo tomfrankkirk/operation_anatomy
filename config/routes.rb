e0fd77112468548a3ceceed2d786bb90ced055b9
Tom Kirk <tomfrankkirk@gmail.com>
2017-12-13 22:19:13 +0000

    New routes for define, removing old routes, updating for static.

--- config/routes.rb
+++ config/routes.rb
@@ -8,23 +8,25 @@ Rails.application.routes.draw do
 
    # custom routes
    get    '/teaching',                      to: 'teaching#show'
-   get    '/images/:id',                    to: 'teaching#fetchImage'
-   get    '/teaching/define',               to: 'teaching#define'
+
    post   '/questions',                     to: 'questions#respond'
    get    '/questions',                     to: 'questions#respond'
-   # get    '/topics',                        to: 'topics#index'
-   # get    '/topics/:id',                    to: 'topics#show', as: 'topic'
+   get    '/static/about',                  to: 'static#about'     
    get    '/dictionary_entries',            to: 'dictionary_entries#index'
+   get    '/dictionary_entries/define',     to: 'dictionary_entries#define'
+
    post   '/feedback',                      to: 'users#submitFeedback'
    get    '/questions_index',               to: 'questions#index'
+   get    '/static/bugs',                   to: 'static#bugs' 
+
    get    '/users',                         to: 'users#index'
    get    '/users/:id',                     to: 'users#show', as: 'user'
-   get    '/static/bugs',                   to: 'static#bugs' 
-   get    '/teaching/*path',                to: 'teaching#webRotateXML'
    get    '/users/adminMode/:id',           to: 'users#adminMode'
    get    '/users/revisionMode/:id',        to: 'users#revisionMode'
 
+   get    '/teaching/*path',                to: 'teaching#webRotateXML'
+   
    # the root of the app -> straight to all topics
-   root "static#about"
+   root "systems#index"
 
 end
