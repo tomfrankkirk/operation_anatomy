e34e18dda85fafc97d1c55b468398bc741ee30d9
Tom Kirk <tomfrankkirk@gmail.com>
2017-12-13 22:14:09 +0000

    Move define() server side function to dictionary entries

--- app/controllers/dictionary_entries_controller.rb
+++ app/controllers/dictionary_entries_controller.rb
@@ -1,5 +1,27 @@
 class DictionaryEntriesController < EndUserController
-    def index 
-        @entries = DictionaryEntry.order(:title)
-    end 
+
+   def index 
+      @entries = DictionaryEntry.order(:title)
+   end 
+
+   def define
+      respond_to do |format|
+         format.json { 
+
+            # Check the param searchString was successfully received. 
+            if string = params[:searchString]
+               entry = DictionaryEntry.searchForEntry(string)
+               if entry 
+                     render :json => { :definition => entry.definition.downcase, :title => entry.title.capitalize, :example => entry.example.downcase }
+                     return 
+               end 
+            end 
+
+            # If not, return an empty hash 
+            render :json => { :definition => "" }     
+         }
+      end
+   end 
+
+
 end
