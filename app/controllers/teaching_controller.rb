7a47bc1dc925cc611e4861e8609750ee9ad8951a
Tom Kirk <tomfrankkirk@gmail.com>
2017-12-19 22:09:38 +0000

    WR360 has come back online with clever routing helpers

--- app/controllers/teaching_controller.rb
+++ app/controllers/teaching_controller.rb
@@ -1,78 +1,93 @@
 class TeachingController < EndUserController
 
-    def fetchImage
-        fullPathString = expandImagePath(params[:id]) 
-        path = Dir[fullPathString + ".*"]
-        img = File.open(path.first)
-        send_data(img.read)
-        img.close
-    end
+   respond_to :html, :xml, :jpg, :js
 
-    # This method updates teaching page partials via JS ajax requests. 
-    # It is also responsible for setting "level viewed" flags when the user reaches end of level 
-    def show
-        @topic = Topic.find(params[:id]) 
-        @level = params[:forLevel]
+   def fetchImage
+      fullPathString = expandImagePath(params[:id]) 
+      path = Dir[fullPathString + ".*"]
+      img = File.open(path.first)
+      send_data(img.read)
+      img.close
+   end
 
-        # Attempt to get the paths for this topic and level. Returns nil if files not found. 
-        @paths = teachingPagePaths(@topic.shortName, @level)
-        respond_to do |format| 
+   # This method updates teaching page partials via JS ajax requests. 
+   # It is also responsible for setting "level viewed" flags when the user reaches end of level 
+   def show
+      @topic = Topic.find(params[:id]) 
+      @level = params[:forLevel]
 
-            # First page load. Set currentPart = 0 and pass all paths off to the view to render. 
-            # The forward/backwards links are dynamically generated within the view by comparing
-            # currentPart with the total number of parts loaded (the count of paths)
-            format.html {
-                if @paths
-                    @path = @paths.first 
-                    @currentPart = 0
-                    if @currentPart + 1 == @paths.count
-                        current_user.setLevelViewed(@topic.id, @level)
-                    end 
-                else 
-                    # If unsuccessful then render the error message
-                    render 'error'
-                end        
-            }
+      # Attempt to get the paths for this topic and level. Returns nil if files not found. 
+      @paths = teachingPagePaths(@topic.shortName, @level)
 
-            # Simply increment or decrement currentPart and re-load. 
-            format.js {
-                @currentPart = (params[:currentPart]).to_i
-                @path = @paths.find { |p| p['P' + (@currentPart + 1).to_s] != nil }
-                # Check if this is the end of the level, if so set flag on user object if not admin mode
-                if @currentPart.to_i + 1 == @paths.count && !current_user.inAdminMode
-                    current_user.setLevelViewed(@topic.id, @level)
-                end 
-            }
-        end
-    end
+      if @paths
 
-    def webRotateXML
-        respond_to do |format|
-            format.xml { 
-                path = Dir["teaching/" + params[:path] + ".xml"]
-                File.open(path.first, 'r') do |f| 
-                    send_data(f.read)
-                end 
-            } 
+         # Get current part, if it exists, or initialise to 0. 
+         @currentPart = (params[:currentPart]).to_i
+         @currentPart = 0 if @currentPart.nil? 
 
-            format.jpg {
-                path = Dir["teaching/" + params[:path] + ".jpg"]
-                File.open(path.first, 'r') do |f| 
-                    send_data(f.read)
-                end 
-            }
-        end 
-    end 
+         # byebug
+         # Check if this is the end of the level, if so set flag on user object if not admin mode
+         if @currentPart + 1 == @paths.count
+            current_user.setLevelViewed(@topic.id, @level) unless current_user.inAdminMode
+         end 
+
+         # Retrieve the part from the paths array. 
+         @path = @paths[@currentPart]
+         @flatHTMLString = nil 
+
+         if @path.include? '.erb'
+            # We need to prepare the erb here...
+            # Define the local vars here.... 
+            @rawStr = File.read(@path)
+            template = ERB.new(@rawStr)
+            @flatHTMLString = template.result(binding)
+         else 
+            @flatHTMLString = File.read(@path)
+         end 
+
+      else 
+
+         # If unsuccessful then render the error message
+         render 'error'
+      end        
+
+      respond_to do |format| 
+         format.html
+         format.js 
+      end
+   end
+
+   def webrotateXMLJPG
+      # What kind of path are we working with?
+      path = params[:path]
+      path = "teaching/" + params[:path] + ".#{params[:format]}"
+      if File.exist? path 
+         send_file(path)
+      else 
+         render :status => 418
+      end 
+   end 
+
+   def webrotate_assets
+      path = params[:path]    
+      path = "vendor/assets/webrotate/" + path + ".#{params[:format]}"
+      if File.exist? path 
+         send_file(path)
+      else 
+         head 418 
+      end 
+   end
 
    private
 
-   def teachingPagePaths(forTopicName, forLevel)
-      pathStr = 'teaching/' + forTopicName + '/' + forLevel + '/*.html'
-      paths = Dir[pathStr]      
+   # Search for both HTML and ERB pages at the given location. 
+   def teachingPagePaths(topicName, forLevel)
+      pathStr = 'teaching/' + topicName + '/' + forLevel + '/*'
+      paths = Dir[pathStr].select { |f| File.file? f }      
       if paths == []
          return nil 
       else 
-         return paths.sort_by { |s| s[/\d+/].to_i }  # Some funky regexp!
+         return paths.sort_by { |s| s[/\d{2,}/].to_i }  # Some funky regexp!
       end
    end
 
