cbcbb27bd5618fd366ac9f7d4cb237567c747801
Tom <tom@Toms-MacBook-Pro.local>
2018-06-16 15:40:47 +0200

    removed topic index method

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -1,29 +1,33 @@
 # frozen_string_literal: true
 
 class TopicsController < EndUserController
-  # When landing back here from a topic, wipe the topic id from the session.
+
+  # Topics should not be directly indexable: redirect back to systems. 
+  # This is a hangover from the fact that topics are listed as a resource 
+  # in order to have access to their path helpers in other areas. 
   def index
-    session[:topicID] = nil
-    @topics = Topic.all
-    @revisionMode = current_user.revisionMode
-    @admin = current_user.isAdmin && current_user.inAdminMode
+    redirect_to systems_url 
   end
 
-  # If landing back here from a set of questions, wipe the level id from the session.
-  # Then rewrite the id in and continue.
+  # Show levels within a topic. If landing back here from a level then 
+  # wipe the level number from the session. 
   def show
     @topic = Topic.find(params[:id])
     @revisionMode = current_user.revisionMode
+    @admin = (current_user.isAdmin && current_user.inAdminMode)
 
     # If not in revision mode, check max level access, otherwise set as nil.
-    @maxLevelAccess = @revisionMode ? nil : current_user.checkLevelAccess(@topic.id)
-
-    @admin = current_user.isAdmin && current_user.inAdminMode
+    @maxLevelAccess = if @revisionMode then 
+       nil else current_user.checkLevelAccess(@topic.id) end 
     @highestViewed = current_user.getHighestViewedLevel(@topic.id)
+
+    # Prepare items for the table cell view. 
     @levelNames = @topic.levelNames
     @pathRoot = "/teaching?id=#{@topic.id}&forLevel="
     @pathStubs = @topic.shortLevelNames
     @iconStubs = @topic.shortLevelNames
+
+    # Reset the session parameters. 
     session[:forLevel] = nil
     session[:topicID] = @topic.id
   end
