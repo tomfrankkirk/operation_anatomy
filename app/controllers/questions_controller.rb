d1050e9ea161a39305ef7f8ca9c88159b2ca22b4
Tom Kirk <tomfrankkirk@gmail.com>
2018-09-14 21:43:28 +0100

    streamlining

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -6,29 +6,30 @@ class QuestionsController < EndUserController
     if current_user.isAdmin
       @questions = Question.all
     else
-      # flash[:error] = "Sorry, this action is only available to administrators."
       redirect_to root_path
     end
   end
 
   # This handles user interactions with questions. HTML response draws the initial page,
-  # thereafter questions are updated via a JS partial.
+  # thereafter questions are drawn onto the partial via JS.
   def respond
     respond_to do |format|
-      # First response, draw the HTML page.
+      
+      # First response, draw the HTML page with a partial for questions
+      # Prepare the questions on the user object, and save the topic ID and level name
+      # into the session hash. This will be required when later updating via JS. 
       format.html do
-        if @topic = Topic.find(params[:id].to_i)
+        begin 
+          @topic = Topic.find(params[:id].to_i)
           @name = @topic.name
           @levelName = params[:levelName]
           session[:id] = params[:id]
           session[:levelName] = params[:levelName]
-
           current_user.prepareQuestions(@topic.id, @levelName)
-        else
-          # Didn't find the topic for some reason. Flash error?
+        rescue Exception => e
+          puts e.message 
           render 'error'
         end
-        return
       end
 
       # This is the responder for all other responses, it updates the partial on the page.
@@ -36,36 +37,36 @@ class QuestionsController < EndUserController
       format.js do
         # Is the user sending a response through? Check by searching for the forQuestion params
         # If so, log the outcome of the response
-        if qid = params[:forQuestion]
-          if Question.find(qid).respond(params[:userResponse])
-            current_user.incrementCurrentScore
-            end
+        if qid = params[:forQuestion] && Question.find(qid).respond(params[:userResponse])
+          current_user.incrementCurrentScore
         end
 
         # Send the next question, if it exists
-        # If it does not exist then log final score and send off to the db.
         if qID = current_user.sendNextQuestionID
           @question = Question.find(qID)
           render 'respond.js'
           return
-        else
-          if !current_user.inAdminMode || current_user.inRevisionMode
-            if !current_user.hasFinishedQuestions(params[:id], params[:levelName])
-              flash[:errorMessage] = 'Warning, could not save scores for previous level'
-            else
-              score = current_user.getLevelScore(params[:id], params[:levelName])
-              flash[:successMessage] = if score > User::THRESHOLD
-                "Congratulations! You scored #{score}% so the next level is available."
-              else 
-                "You scored #{score}%."
-              end
-            end  
+        end 
+
+        # If it does not exist then log final score and send off to the db.
+        if !current_user.inAdminMode || current_user.inRevisionMode
+          if !current_user.hasFinishedQuestions(params[:id], params[:levelName])
+            flash[:errorMessage] = 'Warning, could not save scores for previous level'
           else
-            flash[:successMessage] = "Raw admin score: #{current_user.currentScore}"
-          end
-          render js: "window.location = 'topics/#{params[:id]}'"
+            score = current_user.getLevelScore(params[:id], params[:levelName])
+            flash[:successMessage] = if score > User::THRESHOLD
+              "Congratulations! You scored #{score}% so the next level is available."
+            else 
+              "You scored #{score}%."
+            end
+          end  
+        else
+          flash[:successMessage] = "Raw admin score: #{current_user.currentScore}"
         end
-      end
-    end
-  end
-end
+
+        render js: "window.location = 'topics/#{params[:id]}'"
+      end 
+
+    end 
+  end 
+end 
